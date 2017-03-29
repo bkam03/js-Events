41184b2401c7a45d6861fc7d3e572e2fae3652b6
bkam03 <bkam03@yahoo.com>
2017-03-28 15:01:20 -1000

    #2

--- js/app.js
+++ js/app.js
@@ -21,6 +21,10 @@ function moreContent() {
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+function lessContent() {
+  var pElem = document.getElementById("less");
+  pElem.style.display = "none";
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
