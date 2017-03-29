483e752a133b108831bdf2f81f433262cc6764c3
bkam03 <bkam03@yahoo.com>
2017-03-28 15:09:52 -1000

    3, its fontSize, not fontsize

--- js/app.js
+++ js/app.js
@@ -29,6 +29,11 @@ function lessContent() {
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
+function zoom() {
+  var p = document.getElementById("biggie");
+  p.style.fontSize = "150%";
+  p.style.color = "red";
+}
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
